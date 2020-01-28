d0a3fef1efc14782f0c808d37f4efc145e29f38d
Ijatuyitemi194@gmail.com <Ijatuyitemi194@gmail.com>
2020-01-28 10:35:46 +0100

    gitignore

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -6,7 +6,7 @@ type Wrapper = ((router: Router) => void);
 export const applyMiddleware = (
     middleware: Wrapper[],
     router: Router
-) => {
+    ) => {
     for(const m of middleware){
         m(router);
     }   
